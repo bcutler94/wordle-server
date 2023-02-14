74b998988939c2604f0a634d14c8d0bc3667a67f
bcutler94 <bcutler94@gmail.com>
2023-02-14 11:51:54 -0500

    start on entry

---
+++ entry/types.go
@@ -0,0 +1,25 @@
+package entry
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type EntryInput struct {
+	Guess string `json:"guess" validate:"required,len=5,alpha"`
+}
+
+type LetterResult string
+
+const (
+	Incorrect LetterResult = "incorrect"
+	Correct   LetterResult = "correct"
+	Exists    LetterResult = "exists"
+)
+
+type Entry struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	PuzzleId primitive.ObjectID `bson:"puzzleId"`
+	UserId   primitive.ObjectID `bson:"userId"`
+	Guesses  [5]struct {
+		Guess  string          `bson:"guess"`
+		Result [5]LetterResult `bson:"result"`
+	} `bson:"guesses"`
+}
